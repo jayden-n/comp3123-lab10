77a24c88202a40d097b61cbd4bb908f0ee23a209
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-11-12 00:36:51 -0500

    refactor (form): create error handling when user submit nothing

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,12 +17,12 @@ const Form: FC<IFormProps> = ({ onSubmit }) => {
 	});
 
 	const handleChange = (
-		// for province dropdown menu too
+		// for province dropdown menu type too
 		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
 	) => {
 		// pull out the name, value
 		const { name, value } = e.target;
-		// ... and pass to "setFormData" state
+
 		setFormData({
 			// (update state objects without directly modifying the original state.)
 			...formData,
@@ -31,10 +31,23 @@ const Form: FC<IFormProps> = ({ onSubmit }) => {
 		// console.log(name);
 	};
 
+	// ERROR HANDLING: IF USER DIDN'T SUBMIT ANYTHING...
+	const isFormValid = () => {
+		// extract the keys (array) of the formData object | treating it as array of keys from "ISubmittedData"
+		const formDataKeys = Object.keys(formData) as (keyof ISubmittedData)[];
+
+		// check if all fields have non-empty values
+		return formDataKeys.every((item) => formData[item].trim() !== "");
+	};
+
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		// prevent refreshing
 		e.preventDefault();
-
+		if (!isFormValid()) {
+			alert("All fields are required. Please fill out the form completely.");
+			// stop passing any data
+			return;
+		}
 		onSubmit(formData);
 	};
 
